95aabda22d56414ca0f877ead6c2db68764b244a
Zachary Barrow <zbarrow17@outlook.com>
2026-02-28 21:57:16 -0500

    Fixed Typo in booklistviewmodel (was PagesSizes changed to PageSizes)

--- Bookstore/Models/ViewModels/BookListViewModel.cs
+++ Bookstore/Models/ViewModels/BookListViewModel.cs
@@ -6,6 +6,6 @@
         public BookGridData CurrentRoute { get; set; } = new BookGridData();
         public int TotalPages { get; set; }
 
-        public int[] PagesSizes => new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        public int[] PageSizes => new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     }
 }
